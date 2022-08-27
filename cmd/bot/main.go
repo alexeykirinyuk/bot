9d63c43aa4da4a8b41c0dc074888f0ec8ce05548
Alexey Kirinyuk <akirinyuk@ozon.ru>
2022-08-27 17:50:15 +0300

    list of products

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/alexeykirinyuk/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
@@ -26,6 +27,8 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	productService := product.NewService()
+
 	updates := bot.GetUpdatesChan(tgbotapi.UpdateConfig{
 		Timeout: 60,
 	})
@@ -40,6 +43,8 @@ func main() {
 		switch update.Message.Command() {
 		case "help":
 			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message, productService)
 		default:
 			defaultBehaviour(bot, update.Message)
 		}
@@ -47,7 +52,20 @@ func main() {
 }
 
 func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help")
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n"+
+			"/list - list products")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	list := productService.List()
+	outputMsg := ""
+	for _, p := range list {
+		outputMsg += p.Title + "\n"
+	}
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsg)
 	bot.Send(msg)
 }
 
---
+++ internal/service/product/models.go
@@ -0,0 +1,13 @@
+package product
+
+type Product struct {
+	Title string
+}
+
+var products []Product = []Product{
+	{Title: "one"},
+	{Title: "two"},
+	{Title: "three"},
+	{Title: "four"},
+	{Title: "five"},
+}

3b6af3199f178a02471109edecdd4a00b31158df
Alexey Kirinyuk <akirinyuk@ozon.ru>
2022-08-27 18:30:45 +0300

    workshop experiments

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -1,9 +1,13 @@
 package commands
 
 import (
+	"log"
+
 	"github.com/alexeykirinyuk/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+var registeredCommands = map[string]func(c *Commander, inputMessage *tgbotapi.Message){}
 
 type Commander struct {
 	bot            *tgbotapi.BotAPI
@@ -15,4 +19,28 @@ func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Comman
 		bot:            bot,
 		productService: productService,
 	}
+}
+
+func (c *Commander) HandleUpdate(update *tgbotapi.Update) {
+	if update.Message == nil {
+		return
+	}
+
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+	cmd, ok := registeredCommands[update.Message.Command()]
+	if ok {
+		cmd(c, update.Message)
+	} else {
+		c.Default(update.Message)
+	}
+
+	// switch update.Message.Command() {
+	// case "help":
+	// 	c.Help(update.Message)
+	// case "list":
+	// 	c.List(update.Message)
+	// default:
+	// 	c.Default(update.Message)
+	// }
 }
